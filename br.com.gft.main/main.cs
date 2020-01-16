554489f2a3e07915738e2ede2da0cf50de4cf726
pablopachecoo <power.xone31@gmail.com>
2020-01-16 13:05:23 -0300

    Exercício 01 da prova em C#

---
+++ br.com.gft.main/main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prova01.br.com.gft.model;
+
+namespace Prova01.br.com.gft.main
+{
+    class main
+    {
+        public static void Main(String[] args)
+        {
+
+            Pessoa joao = new Pessoa("Joao", 15);
+            Pessoa leandro = new Pessoa("Leandro", 21);
+            Pessoa paulo = new Pessoa("Paulo", 17);
+            Pessoa jessica = new Pessoa("Jessica", 18);
+
+            List<Pessoa> Pessoa = new List<Pessoa>
+            {
+                joao,
+                leandro,
+                paulo,
+                jessica
+            };
+
+            int maiorId = 0;
+
+            for (int i = 0; i < Pessoa.Count; i++)
+            {
+                if (Pessoa[i].idade > maiorId)
+                {
+                    maiorId = Pessoa[i].idade;
+
+                }
+                                                                      
+            }
+            Console.WriteLine("A Idade da pessoa mais velha é de: " + maiorId + " Anos");
+            Console.ReadLine();
+        }
+
+
+
+
+    }
+}
---
+++ br.com.gft.model/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prova01.br.com.gft.model
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
